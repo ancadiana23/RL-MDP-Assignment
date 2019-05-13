adde61a19a65d687541a3cc8c025b1db42f853fa
Julio <juliojlgon@gmail.com>
2019-05-13 12:20:00 +0200

    Sarsa

--- learnalg.py
+++ learnalg.py
@@ -6,11 +6,6 @@ import sys
 import utils
 
 
-def print_in_line(episode_i):
-    sys.stdout.write("EPISODE [{0}]   \r".format(episode_i))
-    sys.stdout.flush()
-
-
 # TODO: SAVE the T for episode.
 def q_learning(env, num_episodes: int, q=None, discount_factor=1.0, alpha=0.3, policy=None):
     """
@@ -31,7 +26,7 @@ def q_learning(env, num_episodes: int, q=None, discount_factor=1.0, alpha=0.3, p
 
     # loop for each episode
     for episode in range(num_episodes):
-        print_in_line(episode)
+        utils.print_in_line(episode, num_episodes)
         # initialize the state
         state = env.reset()
         done = False
@@ -57,7 +52,9 @@ def q_learning(env, num_episodes: int, q=None, discount_factor=1.0, alpha=0.3, p
     return q
 
 
-def double_q_learning(env, num_episodes: int, q_A=None, q_B=None, discount_factor=1.0, alpha=0.3, policy_A=None, policy_B=None):
+def double_q_learning(
+    env, num_episodes: int, q_A=None, q_B=None, discount_factor=1.0, alpha=0.3, policy_A=None, policy_B=None
+):
     """
     Double Q-Learning (off-policy control) algorithm implementation as described in
     http://incompleteideas.net/sutton/book/ebook/node65.html.
@@ -78,10 +75,10 @@ def double_q_learning(env, num_episodes: int, q_A=None, q_B=None, discount_facto
     if q_B is None:
         q_B = defaultdict(lambda: np.zeros(env.action_space.n))
         policy_B = utils.make_epsilon_greedy_policy(env.action_space.n, epsilon=0.1, q=q_B)
-        
+
     # loop for each episode
     for episode in range(num_episodes):
-        print_in_line(episode)
+        utils.print_in_line(episode, num_episodes)
         # initialize the state
         state = env.reset()
         done = False
@@ -114,3 +111,50 @@ def double_q_learning(env, num_episodes: int, q_A=None, q_B=None, discount_facto
             t += 1
             state = next_state
     return q_A
+
+
+def sarsa(env, num_episodes: int, q=None, discount_factor=1.0, alpha=0.3, policy=None):
+    """
+    Sarsa (on-policy TD control) algorithm implementation as described in
+    http://incompleteideas.net/sutton/book/ebook/node64.html.
+    :param env: The OpenAI Env used
+    :param num_episodes: Number of episodes to run the algorithm for
+    :param q: Q action state values to start from
+    :param discount_factor: The gamma discount factor
+    :param alpha: The learning rate
+    :param policy: The policy to use during training
+    :return: q the optimal value function
+    """
+    # initialize the action value function
+    if q is None:
+        q = defaultdict(lambda: np.zeros(env.action_space.n))
+        policy = utils.make_epsilon_greedy_policy(env.action_space.n, epsilon=0.1, q=q)
+    # loop for each episode
+    for episode in range(num_episodes):
+        utils.print_in_line(episode, num_episodes)
+        # initialize the state
+        state = env.reset()
+        done = False
+        t = 0
+        # loop for each step in the episode
+        # choose action from state based on the policy
+        action_prob = policy(state)
+        action = np.random.choice(np.arange(len(action_prob)), p=action_prob)
+        while not done:
+            # take a step in the environment using a
+            next_state, reward, done, _ = env.step(action)
+            # Choose action from next_state based on the policy
+            next_action_prob = policy(next_state)
+            next_action = np.random.choice(np.arange(len(next_action_prob)), p=next_action_prob)
+            # Sarsa update for Q function
+            q[state][action] += alpha * (
+                reward + discount_factor * q[next_state][next_action] - q[state][action]
+            )
+            # check for finished episode
+            if done:
+                break
+            # otherwise update state, action and increase the t
+            t += 1
+            state = next_state
+            action = next_action
+    return q
